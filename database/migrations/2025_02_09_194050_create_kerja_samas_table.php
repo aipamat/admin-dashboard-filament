4f53b0c6f155b445ebad9e10b87cd903ee99d88b
arif.rachmatdx <arif.rachmatdx@gmail.com>
2025-02-10 02:44:11 +0700

    Menambah Tabel Pada Database

---
+++ database/migrations/2025_02_09_194050_create_kerja_samas_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kerja_sama', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_mitra');
+            $table->year('tahun_perjanjian');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kerja_sama');
+    }
+};
